ebab38e5d7449de1934a3b6ed06f5262d65193f2
AndreiShpartou <AndreiShpartou@gmail.com>
2024-07-27 12:10:42 +0300

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Andrei Shpartou on 27/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
