aa8bc714678cca4186b57692c1ac34744a17c0af
AndreiShpartou <AndreiShpartou@gmail.com>
2024-09-17 14:09:48 +0300

    adjust navigation bar rendering

--- MyMovies/Scenes/MovieList/View/MovieListViewController.swift
+++ MyMovies/Scenes/MovieList/View/MovieListViewController.swift
@@ -42,6 +42,43 @@ class MovieListViewController: UIViewController {
 extension MovieListViewController {
     private func setupViewController() {
         movieListView.delegate = self
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        // Setting the custom title font
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font: Typography.SemiBold.title,
+            NSAttributedString.Key.foregroundColor: UIColor.textColorWhite
+        ]
+        // Custom left button
+        navigationItem.leftBarButtonItem = createCustomLeftBarButton()
+    }
+}
+
+// MARK: - ActionMethods
+extension MovieListViewController {
+    @objc
+    private func backButtonTapped(_ sender: UIButton) {
+        // Handle back button action
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Helpers
+extension MovieListViewController {
+    private func createCustomLeftBarButton() -> UIBarButtonItem {
+        let leftButton = UIButton(type: .custom)
+        leftButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        leftButton.tintColor = .white
+        leftButton.backgroundColor = .primarySoft
+        leftButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        leftButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        leftButton.layer.cornerRadius = 15
+
+        let leftBarButton = UIBarButtonItem(customView: leftButton)
+
+        return leftBarButton
     }
 }
 
