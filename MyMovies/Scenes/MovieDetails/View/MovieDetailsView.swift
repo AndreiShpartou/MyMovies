f835eb108728a91d78c9598740ea12d49c9549fc
AndreiShpartou <AndreiShpartou@gmail.com>
2024-09-23 14:22:58 +0300

    implement background gradient backdrop view

--- MyMovies/Scenes/MovieDetails/View/MovieDetailsView.swift
+++ MyMovies/Scenes/MovieDetails/View/MovieDetailsView.swift
@@ -13,6 +13,7 @@ final class MovieDetailsView: UIView, MovieDetailsViewProtocol {
     // MARK: - UIComponents
     private let scrollView = UIScrollView()
     private let contentView = UIView()
+    private lazy var backdropImageView: UIImageView = createBackdropImageView()
 
     // MARK: - Init
     override init(frame: CGRect) {
@@ -25,17 +26,66 @@ final class MovieDetailsView: UIView, MovieDetailsViewProtocol {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - LifeCycle
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Adjust the size of the background gradient layer
+        backdropImageView.layer.sublayers?.first?.frame = backdropImageView.bounds
+    }
 }
 
 // MARK: - Setup
 extension MovieDetailsView {
     private func setupView() {
         backgroundColor = .primaryBackground
+
+        addSubviews(scrollView, backdropImageView)
+        scrollView.addSubviews(contentView)
+        scrollView.showsVerticalScrollIndicator = false
+    }
+}
+
+// MARK: - Helpers
+extension MovieDetailsView {
+    private func createBackdropImageView() -> UIImageView {
+        let imageView: UIImageView = .createImageView(contentMode: .scaleAspectFill, image: Asset.DefaultCovers.defaultBackdrop.image)
+        let gradientLayer = CAGradientLayer()
+
+        // Transparent color
+        gradientLayer.colors = [
+            UIColor.primaryBackground.withAlphaComponent(0.9).cgColor,
+            UIColor.primaryBackground.withAlphaComponent(0.95).cgColor
+        ]
+        gradientLayer.locations = [0.0, 0.7]
+        imageView.layer.insertSublayer(gradientLayer, at: 0)
+
+        return imageView
     }
 }
 
 // MARK: - Constraints
 extension MovieDetailsView {
     private func setupConstraints() {
+        setupScrollConstraints()
+        setupMainElementsConstraints()
+    }
+
+    private func setupScrollConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        contentView.snp.makeConstraints { make in
+            make.edges.width.equalToSuperview()
+        }
+    }
+
+    private func setupMainElementsConstraints() {
+        backdropImageView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(safeAreaLayoutGuide)
+            make.top.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.7)
+        }
     }
 }
