0c693934bcaed612a593b1f57e760f0a1fd4a13f
AndreiShpartou <AndreiShpartou@gmail.com>
2024-11-16 12:23:26 +0300

    prepare the pop-up person's cell

---
+++ MyMovies/Scenes/Search/View/CollectionViewCells/PersonCircleCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  PersonCircleCollectionViewCell.swift
+//  MyMovies
+//
+//  Created by Andrei Shpartou on 16/11/2024.
+//
+
+import UIKit
+import Kingfisher
+
+final class PersonCircleCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "PersonCollectionViewCell"
+
+    private let personImageView: UIImageView = .createImageView(
+        contentMode: .scaleAspectFill,
+        clipsToBounds: true,
+        cornerRadius: 25
+    )
+
+    private let nameLabel: UILabel = .createLabel(
+        font: Typography.Medium.title,
+        textAlignment: .center,
+        textColor: .textColorWhiteGrey
+    )
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupView()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Public
+    func configure(with person: PersonViewModelProtocol) {
+        personImageView.kf.setImage(with: person.photo, placeholder: Asset.Avatars.avatarDefault.image)
+        nameLabel.text = person.name
+    }
+}
+
+// MARK: - Setup
+extension PersonCircleCollectionViewCell {
+    private func setupView() {
+        contentView.addSubviews(personImageView, nameLabel)
+    }
+}
+
+// MARK: - Constraints
+extension PersonCircleCollectionViewCell {
+    private func setupConstraints() {
+        personImageView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.width.equalTo(50)
+        }
+
+        nameLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(personImageView.snp.bottom).offset(4)
+        }
+    }
+}
