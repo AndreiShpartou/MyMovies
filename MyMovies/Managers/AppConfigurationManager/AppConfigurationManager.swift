9040a9bfe8f634bd3f5003cd901b33dd9c09e2c0
AndreiShpartou <AndreiShpartou@gmail.com>
2025-05-28 16:38:24 +0300

    fix the poor connection image downloading bug

--- MyMovies/Managers/AppConfigurationManager/AppConfigurationManager.swift
+++ MyMovies/Managers/AppConfigurationManager/AppConfigurationManager.swift
@@ -62,6 +62,8 @@ final class AppConfigurationManager: AppConfigurationManagerProtocol {
         imageCache.memoryStorage.config.totalCostLimit = 100 * 1024 * 1024 // 100 MB
         imageCache.diskStorage.config.sizeLimit = 300 * 1024 * 1024 // 300 MB
         imageCache.diskStorage.config.expiration = .days(7)
+        // setup Kingfisher timeout for image downloading
+        KingfisherManager.shared.downloader.downloadTimeout = 300
     }
 
     func configureDefaultServices() {
